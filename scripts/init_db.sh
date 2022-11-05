f049a986c11ff2ac51333ab4b6a591dda2539d24
EJunger <emmajeane.junger@gmail.com>
2022-11-05 17:30:36 -0400

    test

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -16,7 +16,7 @@ fi
 
 DB_USER=${POSTGRES_USER:=postgres}
 DB_PASSWORD=${POSTGRES_PASSWORD:=password}
-DB_NAME=${POSTGRES_DB:=newsletter-db}
+DB_NAME=${POSTGRES_DB:=database}
 DB_PORT=${POSTGRES_PORT:=5432}
 
 # Skip setup if PG instance found
